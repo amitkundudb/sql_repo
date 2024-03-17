f513ea3f64125be4b4626ea10a28f7f1b4439388
amitkundudb <amit.kundu@diggibyte.com>
2024-03-17 13:39:30 +0530

    SQL Problems n Solutions

--- sql_assignment_1.sql
+++ sql_assignment_1.sql
@@ -1,8 +1,8 @@
---1.Create Database as ecommerce 
+--1.Creating Database as ecommerce 
 
 CREATE DATABASE ecommerce
 
-/* 2.Create 4 tables (gold_member_users, users, sales, product)
+/* 2.Creating 4 tables (gold_member_users, users, sales, product)
 under the above database(ecommerce) */
 
 CREATE TABLE gold_members(userid int,"user_name" char(20),signup_date date)
@@ -10,7 +10,8 @@ CREATE TABLE users(userid int,"user_name" varchar(20),signup_date date)
 CREATE TABLE sales(userid int,"user_name" varchar(20),created_date date,product_id int)
 CREATE TABLE product(product_id int,product_name varchar(20),price int)
 
---3.Insert the values provided above with respective datatypes 
+--3.Insert the values provided above with respective datatypes
+
 INSERT INTO gold_members(userid,"user_name",signup_date) VALUES (001,'John','09-22-2017'), (002,'Mary','04-21-2017')
 
 INSERT INTO users(userid,"user_name",signup_date) VALUES (001,'John','09-02-2014'), (003,'Michel','01-15-2015'), (002,'Mary','04-11-2014')
@@ -45,19 +46,19 @@ SELECT
     COUNT(*) AS record_count
 FROM product;
 
---6.What is the total amount each customer spent on ecommerce company
+--6.Total amount each customer spent on ecommerce company
 
 SELECT sales."user_name",SUM(price) AS Total_spent
    FROM sales right join product on sales.product_id=product.product_id
  GROUP BY sales."user_name"
 
- -- 7.Find the distinct dates of each customer visited the website: 
+ -- 7.Distinct dates of each customer visited the website: 
 
 SELECT DISTINCT s.created_date AS date, u.user_name AS customer_name
 FROM sales s INNER JOIN users u
 ON s.userid=u.userid
 
--- 8.Find the first product purchased by each customer using 3 tables(users, sales, product)
+-- 8.First product purchased by each customer using 3 tables(users, sales, product)
 
 WITH RankedPurchases AS (
     SELECT 
@@ -72,20 +73,20 @@ SELECT customer_name, product_purchased
 FROM RankedPurchases
 WHERE purchase_rank = 1
 
-/*9.What is the most purchased item of each customer and how many times the customer has purchased it: 
+/*9.Most purchased item of each customer and how many times the customer has purchased it: 
 output should have 2 columns count of the items as item_count and customer name*/
 
 SELECT COUNT(*) AS item_count, user_name AS customer_name
 FROM sales
 GROUP BY user_name
 
---10.Find out the customer who is not the gold_member_user
+--10.Customer who is not the gold_member_user
 
 SELECT u.user_name AS customer_name
 FROM users u LEFT JOIN gold_members g ON u.userid=g.userid
 WHERE g.userid IS NULL
 
---11.What is the amount spent by each customer when he was the gold_member user
+--11.Amount spent by each customer when he was the gold_member user
 
 SELECT 
     g.user_name AS customer_name, 
@@ -100,15 +101,70 @@ INNER JOIN
 GROUP BY 
     g.user_name;
 
---12.Find the Customers names whose name starts with M
+--12.Finding the Customers names whose name starts with M
 
 SELECT user_name
 FROM users
 WHERE user_name LIKE 'M%'
 
---13.Find the Distinct customer Id of each customer
+--13.Finding the Distinct customer Id of each customer
 
 SELECT DISTINCT userid AS customer_id, user_name
 FROM users
 
---
+--14.Changing the Column name from product table as price_value from price
+EXEC sp_rename 'product.price','price_value','COLUMN'
+
+--15.Changing the Column value product_name – Ipad to Iphone from product table
+
+UPDATE product
+SET product_name = 'Iphone'
+WHERE product_name = 'Ipad'
+
+--16.Changing the table name of gold_member_users to gold_membership_users
+
+EXEC sp_rename 'gold_members','gold_memberships_users'
+
+/*17.Creating a new column as Status in the table crate above gold_membership_users 
+the Status values should be 2 Yes and No 
+if the user is gold member, then status should be Yes else No.*/
+
+ALTER TABLE gold_membership_users
+ADD status VARCHAR(3) DEFAULT 'No'
++++++++++++
+UPDATE gold_membership_users
+SET status = CASE
+     WHEN signup_date IS NOT NULL THEN 'Yes'
+     ELSE 'No'
+END;
+++++++
+SELECT u.user_name,COALESCE(g.status, 'No') AS status
+FROM users u LEFT JOIN gold_membership_users g ON u.userid = g.userid
+
+/*18.Delete the users_ids 1,2 from users table and roll the back changes 
+once both the rows are deleted one by one mention the result when performed roll back*/
+
+BEGIN TRANSACTION
+
+DELETE FROM users
+WHERE userid = 1
+
+DELETE FROM users
+WHERE userid = 2
+
+SELECT * FROM users
+
+ROLLBACK
+
+--19.Insert one more record as same (3,'Laptop',330) as product table
+
+INSERT INTO product VALUES(3,'Laptop',330)
+
+SELECT * FROM product
+
+--20.Query to find the duplicates in product table
+
+SELECT product_id,product_name, COUNT(*) AS duplicate_count
+FROM product
+GROUP BY product_id,product_name
+HAVING count(*)>1
